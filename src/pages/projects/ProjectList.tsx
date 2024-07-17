23d0fa9e0e859ef7229d675cc3c884dfba7ff2f6
kwaneung <kwaneung.kim@outlook.com>
2024-07-18 00:49:42 +0900

    feat: type interface 폴더 init

--- src/pages/projects/ProjectList.tsx
+++ src/pages/projects/ProjectList.tsx
@@ -1,3 +1,35 @@
-const ProjectList = () => <section>프로젝트 리스트 페이지</section>;
+import { useEffect, useState } from 'react';
+
+import type { Project } from '@customTypes/project';
+import { requiredJwtTokeninstance } from '@libs/axios/axios';
+import { AxiosResponse } from 'axios';
+
+interface AxiosRes<ResponseType> {
+  message: string;
+  result: boolean;
+  value: ResponseType;
+}
+
+const ProjectList = () => {
+  const [projectList, setProjectList] = useState<Project[] | null>(null);
+
+  useEffect(() => {
+    const getProjectList = async () => {
+      const response: AxiosResponse<
+        AxiosRes<Project[]>,
+        any
+      > = await requiredJwtTokeninstance.get('/api/user/project/get');
+      return response.data.value;
+    };
+    getProjectList().then((data) => {
+      if (data) setProjectList(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log('projectList : ', projectList);
+  }, [projectList]);
+  return <section>프로젝트 리스트 페이지</section>;
+};
 
 export default ProjectList;
