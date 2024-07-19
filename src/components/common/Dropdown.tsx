9b23c1c073966b5f8cc6b5ceef10ab01d65d8704
kwaneung <kwaneung.kim@outlook.com>
2024-07-19 21:58:59 +0900

    fix: lint 오류 수정

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
+
 import styled from 'styled-components';
-import DropdownPortal from './DropdownPortal';
-import { ReactElement } from 'react';
 
 const ClickEventContainer = styled.div`
   display: flex;
