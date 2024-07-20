1f180f938ab877b58e14b5ab4c32695b50bc9c62
kwaneung <kwaneung.kim@outlook.com>
2024-07-20 09:25:56 +0900

    feat: 스케치 완료

--- src/components/project/ProjectListItem.tsx
+++ src/components/project/ProjectListItem.tsx
@@ -1,9 +1,9 @@
 import { Project } from '@customTypes/project';
+import dayjs from 'dayjs';
 import styled from 'styled-components';
 
 const Card = styled.header`
   display: flex;
-  flex-wrap: nowrap;
   flex-direction: row;
   align-items: center;
   padding: 1rem;
@@ -11,31 +11,69 @@ const Card = styled.header`
   box-sizing: border-box;
   margin-bottom: -0.1rem;
   height: 3rem;
-`;
-
-const Title = styled.div`
-  flex-grow: 2;
-  font-size: 1.2rem;
-  font-weight: bold;
-  min-width: 10rem;
-`;
-
-const Content = styled.div`
-  flex-grow: 10;
-  font-size: 1rem;
+  font-size: 0.8rem;
+  overflow: hidden;
 `;
 
 const Checkbox = styled.input`
   margin-right: 1.5rem;
 `;
 
+const Title = styled.div`
+  font-weight: bold;
+  flex: 3 1 30rem;
+  overflow: hidden;
+  // text-overflow: ellipsis;
+  // white-space: nowrap;
+`;
+
+const Content = styled.div`
+  flex: 3 1 30rem;
+  overflow: hidden;
+  margin-right: 1rem;
+  // text-overflow: ellipsis;
+  // white-space: nowrap;
+`;
+
+const Progress = styled.div`
+  color: grey;
+  flex: 3 1 30rem;
+  margin-right: 1rem;
+`;
+
+const Manager = styled.div`
+  color: grey;
+  flex: 1 1 10rem;
+  margin-right: 1rem;
+`;
+
+const ProjectTimeline = styled.div`
+  color: grey;
+  flex: 2 1 20rem;
+  margin-right: 1rem;
+`;
+
+const SettingsButton = styled.div`
+  color: grey;
+`;
+
 const ProjectListItem = ({ project }: { project: Project }) => {
   console.log('입력받은 project : ', project);
+  const startDate = dayjs(project.startDate).format('YYYY.MM.DD');
+  const endDate = dayjs(project.endDate).format('YYYY.MM.DD');
+  console.log('startDate : ', startDate);
+  console.log('endDate : ', endDate);
   return (
     <Card>
       <Checkbox type="checkbox" />
       <Title>{project.title}</Title>
       <Content>{project.description}</Content>
+      <Progress>진행률 프로그레스바 컴포넌트 위치</Progress>
+      <Manager>담당자 표시 컴포넌트 위치</Manager>
+      <ProjectTimeline>
+        {startDate} ~ {endDate}
+      </ProjectTimeline>
+      <SettingsButton>...</SettingsButton>
     </Card>
   );
 };
--- src/pages/projects/ProjectList.tsx
+++ src/pages/projects/ProjectList.tsx
@@ -25,33 +25,57 @@ const RightSection = styled.div`
 
 const Card = styled.header`
   display: flex;
-  flex-wrap: nowrap;
   flex-direction: row;
   align-items: center;
   padding: 1rem;
-  background-color: #f5f5f5;
   border: 0.1rem solid black;
   box-sizing: border-box;
   margin-bottom: -0.1rem;
   height: 3rem;
-`;
-
-const Title = styled.div`
-  flex-grow: 2;
   font-size: 0.8rem;
-  color: grey;
-  font-weight: bold;
-  min-width: 10rem;
-`;
-
-const Content = styled.div`
-  flex-grow: 10;
-  font-size: 0.8rem;
-  color: grey;
+  overflow: hidden;
 `;
 
 const Checkbox = styled.input`
   margin-right: 1.5rem;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  flex: 3 1 30rem;
+  overflow: hidden;
+  // text-overflow: ellipsis;
+  // white-space: nowrap;
+`;
+
+const Content = styled.div`
+  flex: 3 1 30rem;
+  overflow: hidden;
+  margin-right: 1rem;
+  // text-overflow: ellipsis;
+  // white-space: nowrap;
+`;
+
+const Progress = styled.div`
+  color: grey;
+  flex: 3 1 30rem;
+  margin-right: 1rem;
+`;
+
+const Manager = styled.div`
+  color: grey;
+  flex: 1 1 10rem;
+  margin-right: 1rem;
+`;
+
+const ProjectTimeline = styled.div`
+  color: grey;
+  flex: 2 1 20rem;
+  margin-right: 1rem;
+`;
+
+const SettingsButton = styled.div`
+  color: grey;
 `;
 
 const ProjectList = () => {
@@ -80,6 +104,10 @@ const ProjectList = () => {
         <Checkbox type="checkbox" />
         <Title>프로젝트명</Title>
         <Content>설명</Content>
+        <Progress>진행률</Progress>
+        <Manager>담당자</Manager>
+        <ProjectTimeline>기간</ProjectTimeline>
+        <SettingsButton>...</SettingsButton>
       </Card>
       <main>
         {projectList?.map((project) => (
